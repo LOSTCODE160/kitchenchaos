edf078e5d33ed28c83a993962a2df35b3d05e502
vithal <vithalchoudhary4@gmail.com>
2025-02-24 20:06:03 +0530

    player script

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -5,7 +5,28 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     private void Update()
-    {
+    { Vector2 inputvector = new Vector2(0,0);
+        if (Input.GetKey(KeyCode.W)) 
+        {
+            inputvector.y = 1;
+        };
+        if (Input.GetKey(KeyCode.A)) 
+        {
+            inputvector.x = -1;
+        };
         
+        if (Input.GetKey(KeyCode.S)) 
+        {
+            inputvector.y = -1;
+        };
+        
+        if (Input.GetKey(KeyCode.D)) 
+        {
+            inputvector.x = 1;
+        };
+         inputvector = inputvector.normalized;
+        transform.position += (Vector3)inputvector;
+        Debug.Log(inputvector);
+
     }
 }
