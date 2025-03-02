c102d21d5431937903dccee62a63049aa1d95e7c
vithal <vithalchoudhary4@gmail.com>
2025-03-02 19:07:05 +0530

    script

--- Assets/_scripts_/playeranimator.cs
+++ Assets/_scripts_/playeranimator.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class playeranimator : MonoBehaviour
 {   
-    private const string IS_Walking = "IsWalking";
+    private const string IS_WALKING = "IsWalking";
     [SerializeField]
     private player _player;
     private Animator animator;
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        animator.SetBool(IS_WALKING, _player.IsWalking());
     }
     private void Update()
     {
-       // animator.SetBool(Is_Walking, _player.IsWalking());
+        
     }
 }
